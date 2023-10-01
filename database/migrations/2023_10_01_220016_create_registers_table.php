a40014b4bf7bfcb43e2064dc731e1a823b41d4fb
Charles Ngalasa <charlesngalasa20@gmail.com>
2023-10-02 00:27:00 +0200

    project set up

---
+++ database/migrations/2023_10_01_220016_create_registers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('grade_id');
+            $table->unsignedBigInteger('class_id');
+            $table->text('comment')->nullable();
+            $table->timestamps();
+            $table->foreign('grade_id')->references('id')->on('grades');
+            $table->foreign('class_id')->references('id')->on('classes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registers');
+    }
+};
